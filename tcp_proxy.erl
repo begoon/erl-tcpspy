07b5da586a1a6399708a6d0d881ed5c473267076
Alexander <alexander@demin.ws>
2011-11-30 10:07:27 +0000

    ':' is replaced to '-' to avoid problems on Windows.

--- tcp_proxy.erl
+++ tcp_proxy.erl
@@ -34,7 +34,7 @@ wait() -> receive _ -> true end, wait().
 
 format_socket_info(Info) ->
     {ok, {{A, B, C, D}, Port}} = Info,
-    lists:flatten(io_lib:format("~p.~p.~p.~p:~p", [A, B, C, D, Port])).
+    lists:flatten(io_lib:format("~p.~p.~p.~p-~p", [A, B, C, D, Port])).
 
 peer_info(Socket) -> format_socket_info(inet:peername(Socket)).
 
