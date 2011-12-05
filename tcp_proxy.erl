b620f29a7dac58839bf219ef0a6a5a3e70332dd2
Alexander <alexander@demin.ws>
2011-12-05 01:33:41 +0000

    Almost wokring version.

--- tcp_proxy.erl
+++ tcp_proxy.erl
@@ -4,8 +4,6 @@
 -module(tcp_proxy).
 -export([]).
 
--define(WIDTH, 16).
-
 main([ListenPort, RemoteHost, RemotePort]) ->
      ListenPortN = list_to_integer(ListenPort),
      start(ListenPortN, RemoteHost, list_to_integer(RemotePort));
@@ -20,16 +18,17 @@ usage() ->
 start(ListenPort, CalleeHost, CalleePort) ->
     io:format("Start listening on port ~p and forwarding data to ~s:~p~n",
               [ListenPort, CalleeHost, CalleePort]),
-    %register(binary_logger, spawn(fun() -> binary_logger0() end)),
-    {ok, ListenSocket} = gen_tcp:listen(ListenPort, [binary, {packet, 0}, 
-                                                    {reuseaddr, true}, 
-                                                    {active, true}]),
-    io:format("Listener socket is started ~s~n", [socket_info(ListenSocket)]),
-    spawn(fun() -> acceptor(ListenSocket, CalleeHost, CalleePort, 0) end),
-    wait().
-
-% Infinine loop to make sure that the main thread doesn't exit.
-wait() -> receive _ -> true end, wait().
+    ListenOptions = [binary, {packet, 0}, {reuseaddr, true}, {active, true}],
+    case gen_tcp:listen(ListenPort, ListenOptions) of
+        {ok, ListenSocket} ->
+            io:format("Listener started ~s~n", [socket_info(ListenSocket)]),
+            spawn(fun() -> 
+                acceptor(ListenSocket, CalleeHost, CalleePort, 0) 
+            end),
+            receive _ -> void end;
+        {error, Reason} ->
+            io:format("Unable to start listener, error '~p'~n", [Reason])
+    end.
 
 format_socket_info(Info) ->
     {ok, {{A, B, C, D}, Port}} = Info,
@@ -47,127 +46,165 @@ format_date_time({{Y, M, D}, {H, MM, S}}) ->
 format_duration({Days, {H, M, S}}) ->
     lists:flatten(
        io_lib:format("~2.10.0B-~2.10.0B.~2.10.0B.~2.10.0B", [Days, H, M, S])).
-  
+
 acceptor(ListenSocket, RemoteHost, RemotePort, ConnN) ->
     case gen_tcp:accept(ListenSocket) of
       {ok, LocalSocket} -> 
-          spawn(fun() -> acceptor(ListenSocket, RemoteHost, RemotePort, ConnN + 1) end),
-          LocalInfo = peer_info(LocalSocket),
+          spawn(fun() -> 
+              acceptor(ListenSocket, RemoteHost, RemotePort, ConnN + 1) 
+          end),
           case gen_tcp:connect(RemoteHost, RemotePort, [binary, {packet, 0}]) of
               {ok, RemoteSocket} ->
-                  RemoteInfo = peer_info(RemoteSocket),
-                  Logger = start_connection_logger(ConnN, LocalInfo, RemoteInfo), 
-                  StartTime = calendar:local_time(),
-                  logger ! {message, "~4.10.0B: Connected to ~s at ~s~n", 
-                            [ConnN, RemoteInfo, format_date_time(StartTime)]},
-                  passthrough_data_between(LocalSocket, RemoteSocket, Logger),
-                  EndTime = calendar:local_time(),
-                  Duration = calendar:time_difference(StartTime, EndTime),
-                  logger ! {message, "~4.10.0B: Finished at ~s, duration ~s~n", 
-                            [ConnN, format_date_time(EndTime), 
-                             format_duration(Duration)], ConnN},
-              logger ! stop;
+                  process_connection(ConnN, LocalSocket, RemoteSocket);
               {error, Reason} ->
-                  io:format("Unable to connect to ~s:~s (error: ~p)", 
+                  io:format("Unable to connect to ~s:~s (error: '~p')", 
                             [RemoteHost, RemotePort, Reason])
           end;
       {error, Reason} ->
           io:format("Socket accept error '~w'~n", [Reason])
     end.
 
-passthrough_between(LocalSocket, RemoteSocket, Logger) ->
-    passthrough_between(LocalSockeet, RemoteSocket, Logger, 0).
+-record(loggers, {logger, localBinaryLogger, remoteBinaryLogger}).
 
-passthrough_between(LocalSocket, RemoteSocket, Logger, PacketN) ->
+process_connection(ConnN, LocalSocket, RemoteSocket) ->
+    LocalInfo = peer_info(LocalSocket),
+    RemoteInfo = peer_info(RemoteSocket),
+    Logger = start_connection_logger(ConnN, LocalInfo, RemoteInfo),
+    LocalBinaryLogger = start_binary_logger(ConnN, LocalInfo),
+    RemoteBinaryLogger = start_binary_logger(ConnN, RemoteInfo),
+    StartTime = calendar:local_time(),
+    Logger ! {connected, StartTime},
+    Loggers = #loggers{
+        logger = Logger,
+        localBinaryLogger = LocalBinaryLogger,
+        remoteBinaryLogger = RemoteBinaryLogger
+    },
+    pass_through(LocalSocket, RemoteSocket, Loggers, 0),
+    EndTime = calendar:local_time(),
+    Logger ! {finished, StartTime, EndTime},
+    % Stop the logger.
+    Logger ! {stop, self(), ack},
+    receive ack -> void end,
+    % Stop the binary logger for local socket.
+    LocalBinaryLogger ! {stop, self(), ack},
+    receive ack -> void end,
+    % Stop the binary logger for remote socket.
+    RemoteBinaryLogger ! {stop, self(), ack},
+    receive ack -> void end.
+
+pass_through(LocalSocket, RemoteSocket, Loggers, PacketN) ->
     receive
-        {tcp, RemoteSocket, Packet} ->
-            logger ! {received, from_remote, Packet, PacketN},
-            gen_tcp:send(LocalSocket, Packet),
-            logger ! {sent, to_local, PacketN},
-            passthrough_between(LocalSocket, RemoteSocket, Logger, PacketN + 1);
         {tcp, LocalSocket, Packet} ->
-            logger ! {received, from_local, Packet, PacketN},
-            gen_tcp:send(RemoteSocket, Bin),
-            logger ! {sent, to_remote, PacketN},
-            passthrough_between(LocalSocket, RemoteSocket, Logger, PacketN + 1);
+            Loggers#loggers.logger ! {received, from_local, Packet, PacketN},
+            gen_tcp:send(RemoteSocket, Packet),
+            Loggers#loggers.logger ! {sent, to_remote, PacketN},
+            Loggers#loggers.localBinaryLogger ! {Packet},
+            pass_through(LocalSocket, RemoteSocket, Loggers, PacketN + 1);
+        {tcp, RemoteSocket, Packet} ->
+            Loggers#loggers.logger ! {received, from_remote, Packet, PacketN},
+            gen_tcp:send(LocalSocket, Packet),
+            Loggers#loggers.logger ! {sent, to_local, PacketN},
+            Loggers#loggers.remoteBinaryLogger ! {Packet},
+            pass_through(LocalSocket, RemoteSocket, Loggers, PacketN + 1);
         {tcp_closed, RemoteSocket} -> 
-            logger ! {message, "~4.10.0B: Disconnected from ~s~n", [ConnN, RemoteInfo], ConnN};
+            Loggers#loggers.logger ! {disconnected, from_remote};
         {tcp_closed, LocalSocket} -> 
-            logger ! {message, "~4.10.0B: Disconnected from ~s~n", [ConnN, LocalInfo], ConnN}
+            Loggers#loggers.logger ! {disconnected, from_local}
     end.
 
-% ------------------------------------------------------------------------------------------
+start_binary_logger(ConnN, From) ->
+    {{Y, M, D}, {H, MM, S}} = calendar:local_time(),
+    LogName = lists:flatten(
+                  % YYYY.MM.DD-hh.hh.ss-ConnN-From.log
+                  io_lib:format("log-binary-~4.10.0B.~2.10.0B.~2.10.0B-"
+                                "~2.10.0B.~2.10.0B.~2.10.0B-~4.10.0B-"
+                                "~s.log", 
+                                [Y, M, D, H, MM, S, ConnN, From])),
+    LogWriter = fun(Packet) ->
+        append_to_binary_file(Packet, LogName)
+    end,
+    spawn_link(fun() -> binary_logger(LogWriter) end).
 
-% Binary logger
+binary_logger(LogWriter) ->
+    receive
+        {stop, CallerPid, Ack} ->
+            CallerPid ! Ack;
+        {Packet} ->
+            LogWriter(Packet),
+            binary_logger(LogWriter)
+    end.
 
-append_to_binary_log_file(Bin, ConnN, From, LogNameFormat) ->
-    LogName = io_lib:format(LogNameFormat, [From, ConnN]),
-    {ok, File} = file:open(LogName, [write, append, binary, raw]),
-    file:write(File, Bin),
+append_to_binary_file(Packet, FileName) ->
+    {ok, File} = file:open(FileName, [write, append, binary, raw]),
+    file:write(File, Packet),
     file:close(File).
 
-binary_logger0() ->
-    {{Y, M, D}, {H, MM, S}} = calendar:local_time(),
-    LogNameFormat = lists:flatten(io_lib:format(?LogNameTemplate, 
-                                                ["binary-~s-", Y, M, D, H, MM, S])),
-    LogNameExample = io_lib:format(LogNameFormat, ["host", 0]),
-    io:format("Started file binary logger to [~s]~n", [LogNameExample]),
-    binary_logger(LogNameFormat).
-
-binary_logger(LogNameFormat) ->
-    receive
-        {Bin, ConnN, From} ->
-            append_to_binary_log_file(Bin, ConnN, From, LogNameFormat),
-            binary_logger(LogNameFormat)
-    end.
-
-% Logger thread
-
-% This function launches pre-connection dump logger.
 start_connection_logger(ConnN, From, To) ->
     {{Y, M, D}, {H, MM, S}} = calendar:local_time(),
     LogName = lists:flatten(
                   % YYYY.MM.DD-hh.hh.ss-ConnN-From-To.log
                   io_lib:format("log-~4.10.0B.~2.10.0B.~2.10.0B-"
                                 "~2.10.0B.~2.10.0B.~2.10.0B-~4.10.0B-"
-                                "~s-~s.log")., 
+                                "~s-~s.log", 
                                 [Y, M, D, H, MM, S, ConnN, From, To])),
-    spawn(fun() -> connection_logger(ConnN, From, To, LogName) end).
+    spawn_link(fun() -> connection_logger(ConnN, From, To, LogName) end).
 
-connection_logger(ConnN, Origin, Dest, LogName) ->
+peertype_to_name(from_local, LocalInfo, _RemoteInfo) -> LocalInfo;
+peertype_to_name(from_remote, _LocalInfo, RemoteInfo) -> RemoteInfo;
+peertype_to_name(to_local, LocalInfo, _RemoteInfo) -> LocalInfo;
+peertype_to_name(to_remote, _LocalInfo, RemoteInfo) -> RemoteInfo.
+
+connection_logger(ConnN, From, To, LogName) when is_list(LogName) ->
+    Putter = fun(Message) ->
+        append_message_to_file(Message, LogName)
+    end,
+    connection_logger(ConnN, From, To, Putter);
+
+connection_logger(ConnN, FromInfo, ToInfo, LogWriter) ->
     receive
         {received, From, Packet, PacketN} ->
-            FromInfo = if 
-                From == from_local -> Origin;
-                From == from_remote -> Dest
+            PeerName = peertype_to_name(From, FromInfo, ToInfo),
+            Message = fun(Printer) -> 
+                Printer("Received (#~p) ~p byte(s) from ~s~n", 
+                        [PacketN, byte_size(Packet), PeerName]),
+                binary_to_hex_via_printer(Packet, Printer)
             end,
-            put_log_entry(
-                fun(Printer) -> 
-                    Printer("~4.10.0B: Received (#~p) ~p byte(s) from ~s~n", 
-                            [ConnN, PacketN, byte_size(Packet), FromInfo]),
-                    binary_to_hex_via_printer(Packet, Printer, ConnN)
-                end,
-                LogName),
-            %binary_logger ! {Packet, ConnN, From},
-            connection_logger(ConnN, Origin, Dest, LogName);
+            write_message(ConnN, FromInfo, ToInfo, LogWriter, Message);
         {sent, To, PacketN} ->
-            ToInfo = if 
-                To == to_local -> Origin;
-                To == to_remote -> Dest
+            PeerName = peertype_to_name(To, FromInfo, ToInfo),
+            Message = fun(Printer) ->
+                Printer("Sent (#~p) to ~s~n", [PacketN, PeerName])
             end,
-            put_log_entry(
-                fun(Printer) -> 
-                    Printer("~4.10.0B: Sent (#~p) to ~s~n", [ConnN, PacketN, ToInfo]),
-                end,
-                LogName);
-            connection_logger(ConnN, Origin, Dest, LogName);
-        {message, Format, Args} ->
-                fun(Printer) -> Printer(Format, Args) end,
-            connection_logger(ConnN, Origin, Dest, LogName);            
-        exit -> ok
-    end
+            write_message(ConnN, FromInfo, ToInfo, LogWriter, Message);
+        {connected, Time} ->
+            When = format_date_time(Time),
+            Message = fun(Printer) ->
+                Printer("Connected to ~s at ~s~n", [ToInfo, When])
+            end,
+            write_message(ConnN, FromInfo, ToInfo, LogWriter, Message);
+        {disconnected, From} ->
+            PeerName = peertype_to_name(From, FromInfo, ToInfo),
+            Message = fun(Printer) ->
+                Printer("Disconnected from ~s~n", [PeerName])
+            end,
+            write_message(ConnN, FromInfo, ToInfo, LogWriter, Message);
+        {finished, StartTime, EndTime} ->
+            Duration = calendar:time_difference(StartTime, EndTime),
+            When = format_date_time(EndTime),
+            Message = fun(Printer) ->
+                Printer("Finished at ~s, duration ~s~n",
+                        [When, format_duration(Duration)])
+            end,
+            write_message(ConnN, FromInfo, ToInfo, LogWriter, Message);
+        {stop, CallerPid, Ack} ->
+            CallerPid ! Ack 
+    end.
 
-put_log_entry(Putter, LogName) ->
+write_message(ConnN, FromInfo, ToInfo, LogWriter, Message) ->
+   LogWriter(Message),
+   connection_logger(ConnN, FromInfo, ToInfo, LogWriter).
+
+append_message_to_file(Putter, LogName) ->
     {_, File} = file:open(LogName, [write, append]),
     Printer = fun(Format, Args) ->
         io:format(Format, Args),
@@ -178,30 +215,29 @@ put_log_entry(Putter, LogName) ->
        
 % -----------------------------------------------------------------------------
 
-binary_to_hex_via_console(Bin, Prefix) ->
-    binary_to_hex_via_printer(Bin, fun(F, P) -> io:format(F, P) end, Prefix).
+-define(WIDTH, 32).
 
-binary_to_hex_via_printer(Bin, Printer, Prefix) ->
-    binary_to_hex(Bin, Printer, 0, io_lib:format("~4.10.0B: ", [Prefix])).
+binary_to_hex_via_printer(Bin, Printer) ->
+    binary_to_hex(Bin, Printer, 0).
 
-binary_to_hex(<<Bin:16/binary, Rest/binary>>, Printer, Offset, Prefix) ->
-    binary_to_dump_line(Bin, Printer, Offset, Prefix),
-    binary_to_hex(Rest, Printer, Offset + 16, Prefix);
+binary_to_hex(<<Bin:?WIDTH/binary, Rest/binary>>, Printer, Offset) ->
+    binary_to_dump_line(Bin, Printer, Offset),
+    binary_to_hex(Rest, Printer, Offset + ?WIDTH);
 
-binary_to_hex(Bin, Printer, Offset, Prefix) ->
-    Pad = fun() -> Printer("~*c", [(16 - byte_size(Bin)) * 3, 32]) end,
-    binary_to_dump_line(Bin, Printer, Pad, Offset, Prefix).
+binary_to_hex(Bin, Printer, Offset) ->
+    Pad = fun() -> Printer("~*c", [(?WIDTH - byte_size(Bin)) * 3, 32]) end,
+    binary_to_dump_line(Bin, Printer, Pad, Offset).
 
-binary_to_dump_line(Bin, Printer, Offset, Prefix) ->
-    binary_to_dump_line(Bin, Printer, fun() -> ok end, Offset, Prefix).
+binary_to_dump_line(Bin, Printer, Offset) ->
+    binary_to_dump_line(Bin, Printer, fun() -> ok end, Offset).
 
-binary_to_dump_line(Bin, Printer, Pad, Offset, Prefix) ->
-    Printer("~s~4.16.0B: ", [Prefix, Offset]),
+binary_to_dump_line(Bin, Printer, Pad, Offset) ->
+    Printer("~4.16.0B: ", [Offset]),
     Printer("~s", [binary_to_hex_line(Bin)]),
     Pad(),
     Printer("| ~s~n", [binary_to_char_line(Bin)]).
 
-binary_to_hex_line(Bin) -> [[byte_to_hex(<<B>>) ++ " " || << B >> <= Bin]].
+binary_to_hex_line(Bin) -> [[(byte_to_hex(<<B>>) ++ " ") || << B >> <= Bin]].
 
 byte_to_hex(<< N1:4, N2:4 >>) ->
     [integer_to_list(N1, 16), integer_to_list(N2, 16)].
@@ -210,3 +246,4 @@ binary_to_char_line(Bin) -> [[mask_invisiable_chars(B) || << B >> <= Bin]].
 
 mask_invisiable_chars(X) when (X >= 32 andalso X < 128) -> X;
 mask_invisiable_chars(_) -> $..
+
